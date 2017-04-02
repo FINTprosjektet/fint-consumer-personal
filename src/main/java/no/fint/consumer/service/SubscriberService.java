cd8897f2ce126a393307c70cb446471ecc0823c7
jarlehansen <jarle@jarlehansen.net>
2017-04-02 16:22:48 +0200

    simplified the data convertion in SubscriberService

--- src/main/java/no/fint/consumer/service/SubscriberService.java
+++ src/main/java/no/fint/consumer/service/SubscriberService.java
@@ -40,39 +40,32 @@ public class SubscriberService {
         if (event.getAction().equals("GET_ALL_PERSONALRESSURS")) {
             List<?> employees = event.getData();
             List<FintResource> convertedList = employees.stream().map(employee -> objectMapper.convertValue(employee, FintResource.class)).collect(Collectors.toList());
-            List<FintResource<Personalressurs>> personalressursList = new ArrayList<>();
-            for (FintResource<Personalressurs> resource : convertedList) {
-                FintResource<Personalressurs> fintResource = new FintResource<>(Personalressurs.class, resource.getResource());
-                fintResource.setRelasjoner(resource.getRelasjoner());
-                personalressursList.add(fintResource);
-            }
-
+            List<FintResource<Personalressurs>> personalressursList = mapFintResource(Personalressurs.class, convertedList);
             personalressursCacheService.getCache(CacheUri.create(event.getOrgId(), "personalressurs")).ifPresent(cache -> cache.update(personalressursList));
         } else if (event.getAction().equals("GET_ALL_PERSON")) {
             List<?> persons = event.getData();
             List<FintResource> convertedList = persons.stream().map(person -> objectMapper.convertValue(person, FintResource.class)).collect(Collectors.toList());
-            List<FintResource<Person>> personList = new ArrayList<>();
-            for (FintResource<Person> resource : convertedList) {
-                FintResource<Person> fintResource = new FintResource<>(Person.class, resource.getResource());
-                fintResource.setRelasjoner(resource.getRelasjoner());
-                personList.add(fintResource);
-            }
-
+            List<FintResource<Person>> personList = mapFintResource(Person.class, convertedList);
             personCacheService.getCache(CacheUri.create(event.getOrgId(), "person")).ifPresent(cache -> cache.update(personList));
         } else if (event.getAction().equals("GET_ALL_ARBEIDSFORHOLD")) {
             List<?> employments = event.getData();
             List<FintResource> convertedList = employments.stream().map(employment -> objectMapper.convertValue(employment, FintResource.class)).collect(Collectors.toList());
-            List<FintResource<Arbeidsforhold>> arbeidsforholdList = new ArrayList<>();
-            for (FintResource<Arbeidsforhold> resource : convertedList) {
-                FintResource<Arbeidsforhold> fintResource = new FintResource<>(Arbeidsforhold.class, resource.getResource());
-                fintResource.setRelasjoner(resource.getRelasjoner());
-                arbeidsforholdList.add(fintResource);
-            }
-
+            List<FintResource<Arbeidsforhold>> arbeidsforholdList = mapFintResource(Arbeidsforhold.class, convertedList);
             arbeidsforholdCacheService.getCache(CacheUri.create(event.getOrgId(), "arbeidsforhold")).ifPresent(cache -> cache.update(arbeidsforholdList));
         } else {
             log.warn("Unhandled event: {}", event.getAction());
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> List<FintResource<T>> mapFintResource(Class<T> type, List<FintResource> fintResources) {
+        List<FintResource<T>> resources = new ArrayList<>();
+        for (FintResource fintResource : fintResources) {
+            FintResource<T> resource = new FintResource<>(type, (T) fintResource.getResource());
+            resource.setRelasjoner(fintResource.getRelasjoner());
+            resources.add(resource);
+        }
+        return resources;
     }
 
 }
