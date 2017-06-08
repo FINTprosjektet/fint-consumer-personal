0f22aaef9e749b416fd6abcc0e89d61df2904d2c
Jarle Hansen <jarle@jarlehansen.net>
2017-06-08 12:22:33 +0200

    added @EnableSpringfoxExtension

--- src/main/java/no/fint/consumer/Application.java
+++ src/main/java/no/fint/consumer/Application.java
@@ -1,11 +1,12 @@
 package no.fint.consumer;
 
 import com.github.springfox.loader.EnableSpringfox;
+import io.swagger.annotations.*;
 import no.fint.audit.EnableFintAudit;
 import no.fint.dependencies.annotations.EnableFintDependencies;
 import no.fint.events.annotations.EnableFintEvents;
-import no.fint.events.controller.FintEventsController;
 import no.fint.relations.annotations.EnableFintRelations;
+import no.fint.springfox.EnableSpringfoxExtension;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,7 +16,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFintEvents
 @EnableFintAudit
 @EnableScheduling
-@EnableSpringfox(includeControllers = FintEventsController.class)
+@EnableSpringfox(@Info(title = "FINT Consumer Personal", version = "${fint.version}",
+        extensions = {@Extension(name = "x-logo",
+                properties = {@ExtensionProperty(name = "url", value = "/images/logo.png")}
+        )}
+))
+@EnableSpringfoxExtension
+@SwaggerDefinition(externalDocs = @ExternalDocs(value = "Go to the API list", url = "/api"))
 @SpringBootApplication
 public class Application {
 
