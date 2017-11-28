240f496980e5d7d78ee0b866278be2bc2f52b804
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-11-28 08:48:45 +0100

    Small code cleanup.

--- src/main/java/no/fint/consumer/arbeidsforhold/ArbeidsforholdCacheService.java
+++ src/main/java/no/fint/consumer/arbeidsforhold/ArbeidsforholdCacheService.java
@@ -3,7 +3,6 @@ package no.fint.consumer.arbeidsforhold;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.cache.CacheService;
-import no.fint.cache.FintCache;
 import no.fint.consumer.config.Constants;
 import no.fint.consumer.config.ConsumerProps;
 import no.fint.consumer.event.ConsumerEventUtil;
