a2064c802ed5f2e0cc445f5d45c92b927da9b9ae
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-03-14 18:49:31 +0100

    Added roles and authorizations.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,8 +1,12 @@
 package no.fint.consumer.config;
 
 import no.fint.consumer.utils.RestEndpoints;
+
 import java.util.Map;
+
 import com.google.common.collect.ImmutableMap;
+import no.fint.model.administrasjon.fullmakt.Fullmakt;
+import no.fint.model.administrasjon.fullmakt.Rolle;
 import no.fint.model.administrasjon.kodeverk.*;
 import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
 import no.fint.model.administrasjon.personal.*;
@@ -13,27 +17,29 @@ import no.fint.model.felles.kodeverk.iso.Sprak;
 
 public class LinkMapper {
 
-	public static Map<String, String> linkMapper(String contextPath) {
-		return ImmutableMap.<String,String>builder()
-			.put(Arbeidsforhold.class.getName(), contextPath + RestEndpoints.ARBEIDSFORHOLD)
-			.put(Fastlonn.class.getName(), contextPath + RestEndpoints.FASTLONN)
-			.put(Fravar.class.getName(), contextPath + RestEndpoints.FRAVAR)
-			.put(Person.class.getName(), contextPath + RestEndpoints.PERSON)
-			.put(Personalressurs.class.getName(), contextPath + RestEndpoints.PERSONALRESSURS)
-			.put(Variabellonn.class.getName(), contextPath + RestEndpoints.VARIABELLONN)
-			.put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
-			.put(Fravarsgrunn.class.getName(), "/administrasjon/kodeverk/fravarsgrunn")
-			.put(Fravarstype.class.getName(), "/administrasjon/kodeverk/fravarstype")
-			.put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
-			.put(Arbeidsforholdstype.class.getName(), "/administrasjon/kodeverk/arbeidsforholdstype")
-			.put(Stillingskode.class.getName(), "/administrasjon/kodeverk/stillingskode")
-			.put(Uketimetall.class.getName(), "/administrasjon/kodeverk/uketimetall")
-			.put(Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement")
-			.put(Personalressurskategori.class.getName(), "/administrasjon/kodeverk/personalressurskategori")
-			.put(Sprak.class.getName(), "/felles/kodeverk/sprak")
-			.put(Landkode.class.getName(), "/felles/kodeverk/land")
-			.put(Kjonn.class.getName(), "/felles/kodeverk/kjonn")
-			.build();
-	}
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String, String>builder()
+                .put(Arbeidsforhold.class.getName(), contextPath + RestEndpoints.ARBEIDSFORHOLD)
+                .put(Fastlonn.class.getName(), contextPath + RestEndpoints.FASTLONN)
+                .put(Fravar.class.getName(), contextPath + RestEndpoints.FRAVAR)
+                .put(Person.class.getName(), contextPath + RestEndpoints.PERSON)
+                .put(Personalressurs.class.getName(), contextPath + RestEndpoints.PERSONALRESSURS)
+                .put(Variabellonn.class.getName(), contextPath + RestEndpoints.VARIABELLONN)
+                .put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
+                .put(Fravarsgrunn.class.getName(), "/administrasjon/kodeverk/fravarsgrunn")
+                .put(Fravarstype.class.getName(), "/administrasjon/kodeverk/fravarstype")
+                .put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
+                .put(Arbeidsforholdstype.class.getName(), "/administrasjon/kodeverk/arbeidsforholdstype")
+                .put(Stillingskode.class.getName(), "/administrasjon/kodeverk/stillingskode")
+                .put(Uketimetall.class.getName(), "/administrasjon/kodeverk/uketimetall")
+                .put(Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement")
+                .put(Personalressurskategori.class.getName(), "/administrasjon/kodeverk/personalressurskategori")
+                .put(Rolle.class.getName(), "/administrasjon/fullmakt/rolle")
+                .put(Fullmakt.class.getName(), "/administrasjon/fullmakt/fullmakt")
+                .put(Sprak.class.getName(), "/felles/kodeverk/sprak")
+                .put(Landkode.class.getName(), "/felles/kodeverk/land")
+                .put(Kjonn.class.getName(), "/felles/kodeverk/kjonn")
+                .build();
+    }
 
 }
