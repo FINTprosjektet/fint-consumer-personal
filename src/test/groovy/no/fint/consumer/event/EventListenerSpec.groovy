d339c700a46096b892674db1ff76a22cec7d3df4
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-05-28 22:13:06 +0200

    Update spec.

--- src/test/groovy/no/fint/consumer/event/EventListenerSpec.groovy
+++ src/test/groovy/no/fint/consumer/event/EventListenerSpec.groovy
@@ -1,16 +1,20 @@
 package no.fint.consumer.event
 
+import no.fint.audit.FintAuditService
 import no.fint.consumer.status.StatusCache
 import no.fint.event.model.Event
+import no.fint.event.model.Status
 import spock.lang.Specification
 
 class EventListenerSpec extends Specification {
     private EventListener eventListener
     private StatusCache statusCache
+    private FintAuditService fintAuditService
 
     void setup() {
+        fintAuditService = Mock()
         statusCache = Mock(StatusCache)
-        eventListener = new EventListener(cacheServices: [], statusCache: statusCache)
+        eventListener = new EventListener(cacheServices: [], statusCache: statusCache, fintAuditService: fintAuditService)
     }
 
     def "No exception is thrown when receiving event"() {
@@ -20,5 +24,6 @@ class EventListenerSpec extends Specification {
         then:
         noExceptionThrown()
         1 * statusCache.containsKey(_ as String) >> false
+        1 * fintAuditService.audit(_ as Event, _ as Status)
     }
 }
