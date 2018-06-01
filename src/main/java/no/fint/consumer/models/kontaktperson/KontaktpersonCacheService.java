589a8f7920e6b3d539183e2dc8f7f9f20689f145
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-06-01 16:26:41 +0200

    Revert "Revert to 2.7.1"
    This reverts commit 870e77adf8daa81eccf934575dcaf0f6ed25811a.

---
+++ src/main/java/no/fint/consumer/models/kontaktperson/KontaktpersonCacheService.java
@@ -0,0 +1,113 @@
+package no.fint.consumer.models.kontaktperson;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+
+import no.fint.cache.CacheService;
+import no.fint.consumer.config.Constants;
+import no.fint.consumer.config.ConsumerProps;
+import no.fint.consumer.event.ConsumerEventUtil;
+import no.fint.event.model.Event;
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.relations.FintResourceCompatibility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import no.fint.model.felles.Kontaktperson;
+import no.fint.model.resource.felles.KontaktpersonResource;
+import no.fint.model.felles.FellesActions;
+
+@Slf4j
+@Service
+public class KontaktpersonCacheService extends CacheService<KontaktpersonResource> {
+
+    public static final String MODEL = Kontaktperson.class.getSimpleName().toLowerCase();
+
+    @Value("${fint.consumer.compatibility.fintresource:true}")
+    private boolean checkFintResourceCompatibility;
+
+    @Autowired
+    private FintResourceCompatibility fintResourceCompatibility;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private KontaktpersonLinker linker;
+
+    private JavaType javaType;
+
+    private ObjectMapper objectMapper;
+
+    public KontaktpersonCacheService() {
+        super(MODEL, FellesActions.GET_ALL_KONTAKTPERSON, FellesActions.UPDATE_KONTAKTPERSON);
+        objectMapper = new ObjectMapper();
+        javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, KontaktpersonResource.class);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @PostConstruct
+    public void init() {
+        Arrays.stream(props.getOrgs()).forEach(this::createCache);
+    }
+
+    @Scheduled(initialDelayString = ConsumerProps.CACHE_INITIALDELAY_KONTAKTPERSON, fixedRateString = ConsumerProps.CACHE_FIXEDRATE_KONTAKTPERSON)
+    public void populateCacheAll() {
+        Arrays.stream(props.getOrgs()).forEach(this::populateCache);
+    }
+
+    public void rebuildCache(String orgId) {
+		flush(orgId);
+		populateCache(orgId);
+	}
+
+    private void populateCache(String orgId) {
+		log.info("Populating Kontaktperson cache for {}", orgId);
+        Event event = new Event(orgId, Constants.COMPONENT, FellesActions.GET_ALL_KONTAKTPERSON, Constants.CACHE_SERVICE);
+        consumerEventUtil.send(event);
+    }
+
+
+    public Optional<KontaktpersonResource> getKontaktpersonBySystemId(String orgId, String systemId) {
+        return getOne(orgId, (resource) -> Optional
+                .ofNullable(resource)
+                .map(KontaktpersonResource::getSystemId)
+                .map(Identifikator::getIdentifikatorverdi)
+                .map(_id -> _id.equals(systemId))
+                .orElse(false));
+    }
+
+
+	@Override
+    public void onAction(Event event) {
+        List<KontaktpersonResource> data;
+        if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
+            log.info("Compatibility: Converting FintResource<KontaktpersonResource> to KontaktpersonResource ...");
+            data = fintResourceCompatibility.convertResourceData(event.getData(), KontaktpersonResource.class);
+        } else {
+            data = objectMapper.convertValue(event.getData(), javaType);
+        }
+        data.forEach(linker::mapLinks);
+        if (FellesActions.valueOf(event.getAction()) == FellesActions.UPDATE_KONTAKTPERSON) {
+            add(event.getOrgId(), data);
+            log.info("Added {} elements to cache for {}", data.size(), event.getOrgId());
+        } else {
+            update(event.getOrgId(), data);
+            log.info("Updated cache for {} with {} elements", event.getOrgId(), data.size());
+        }
+    }
+}
